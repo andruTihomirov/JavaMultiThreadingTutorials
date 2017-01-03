1f27dbb6e3e9e8c0cd4c6078de4f564409032ede
andru.tihomirov <andru.tihomirov@gmail.com>
2017-01-03 16:20:21 +0300

    Advanced Java: Multi-threading Part 12 - Semaphores https://www.youtube.com/watch?v=KxTRsvgqoVQ&index=12&list=PLBB24CFB073F1048E

---
+++ src/com/multithreading/part12/semaphores/App.java
@@ -0,0 +1,27 @@
+package com.multithreading.part12.semaphores;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by andrei.tsikhamirau on 1/3/2017.
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for(int i = 0; i < 200; i++) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    Connection.getInstance().connect();
+                }
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
