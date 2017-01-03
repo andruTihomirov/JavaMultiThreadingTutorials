85e0bd4fc35b8c8d99eb01c2f056648e223e793f
andru.tihomirov <andru.tihomirov@gmail.com>
2017-01-03 12:33:50 +0300

    Advanced Java: Multi-threading Part 10 - Re-entrant Locks https://www.youtube.com/watch?v=fjMTaVykOpc&list=PLBB24CFB073F1048E&index=10

---
+++ src/com/multithreading/part10/reentrantlocks/App.java
@@ -0,0 +1,39 @@
+package com.multithreading.part10.reentrantlocks;
+
+/**
+ * Created by andrei.tsikhamirau on 1/3/2017.
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Runner runner = new Runner();
+
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.firstThread();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.secondThread();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        runner.finished();
+    }
+}
